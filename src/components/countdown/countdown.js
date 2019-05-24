c1d73f28897c030b69cbd9141e1ffa5f5a591f07
Josh Doan <joshd@TagPay.com>
2019-05-24 16:24:27 -0700

    added disable after submitting a date

--- src/components/countdown/countdown.js
+++ src/components/countdown/countdown.js
@@ -9,7 +9,8 @@ const Countdown = class Countdown extends React.Component {
             days: '',
             hours: '',
             minutes: '',
-            seconds: ''
+            seconds: '',
+            submitted: false
         }
     }
 
@@ -18,16 +19,17 @@ const Countdown = class Countdown extends React.Component {
         };
 
         getDate = () => {
-            this.interval = setInterval(() => {
-                const now = new Date().getTime() / 1000;
-                const date = new Date(this.state.date).getTime() / 1000;
-                const timeLeft = (date - now);
-                const days = Math.floor(timeLeft / 86400);
-                const hours = Math.floor(timeLeft / 3600) % 24;
-                const minutes = Math.floor(timeLeft / 60) % 60;
-                const seconds = Math.floor(timeLeft) % 60;
-                this.setState({ days: days, hours: hours, minutes: minutes, seconds: seconds});
-            }, 1000);
+                this.interval = setInterval(() => {
+                    const now = new Date().getTime() / 1000;
+                    const date = new Date(this.state.date).getTime() / 1000;
+                    const timeLeft = (date - now);
+                    const days = Math.floor(timeLeft / 86400);
+                    const hours = Math.floor(timeLeft / 3600) % 24;
+                    const minutes = Math.floor(timeLeft / 60) % 60;
+                    const seconds = Math.floor(timeLeft) % 60;
+                    this.setState({ days: days, hours: hours, minutes: minutes, seconds: seconds});
+                }, 1000);
+                this.setState({submitted: true})
         };
 
     render() {
@@ -35,8 +37,8 @@ const Countdown = class Countdown extends React.Component {
             <div>
                 <div>{this.state.days} Days {this.state.hours} Hours {this.state.minutes} Minutes {this.state.seconds} Seconds</div>
                 <h3>Enter a date</h3>
-                <input type="date" onChange={this.handleChange} value={this.state.date}></input>
-                <button onClick={this.getDate}>Start</button>
+                <input type="date" disabled={this.state.submitted} onChange={this.handleChange} value={this.state.date}></input>
+                <button disabled={this.state.submitted} onClick={this.getDate}>Start</button>
             </div>
         );
     }
