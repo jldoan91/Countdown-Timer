277596d7ecfc3e5511acb56b4ed5db78683fa090
unknown <jldoan91@gmail.com>
2019-05-28 21:40:27 -0700

    added if check for when timer hits 0 or starts at less than 0

--- src/components/countdown/countdown.js
+++ src/components/countdown/countdown.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Countdown = class Countdown extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             date: '',
@@ -14,28 +14,33 @@ const Countdown = class Countdown extends React.Component {
         }
     }
 
-        handleChange = event => {
-            this.setState({date: event.target.value});
-        };
+    handleChange = event => {
+        this.setState({ date: event.target.value });
+    };
 
-        getDate = () => {
-                this.interval = setInterval(() => {
-                    const now = new Date().getTime() / 1000;
-                    const date = new Date(this.state.date).getTime() / 1000;
-                    const timeLeft = (date - now);
-                    const days = Math.floor(timeLeft / 86400);
-                    const hours = Math.floor(timeLeft / 3600) % 24;
-                    const minutes = Math.floor(timeLeft / 60) % 60;
-                    const seconds = Math.floor(timeLeft) % 60;
-                    this.setState({ days: days, hours: hours, minutes: minutes, seconds: seconds});
-                }, 1000);
-                this.setState({submitted: true})
-        };
+    getDate = () => {
+        this.setState({ submitted: true })
+        this.interval = setInterval(() => {
+            const now = new Date().getTime() / 1000;
+            const date = new Date(this.state.date).getTime() / 1000;
+            const timeLeft = (date - now);
+            const days = Math.floor(timeLeft / 86400);
+            const hours = Math.floor(timeLeft / 3600) % 24;
+            const minutes = Math.floor(timeLeft / 60) % 60;
+            const seconds = Math.floor(timeLeft) % 60;
+            this.setState({ days: days, hours: hours, minutes: minutes, seconds: seconds });
+            if (timeLeft <= 0) {
+                this.setState({ days: 0, hours: 0, minutes: 0, seconds: 0 })
+                this.setState({ submitted: false })
+                clearInterval(this.interval);
+            }
+        }, 1000);
+    };
 
     render() {
         return (
             <div>
-                <div>{this.state.days} Days {this.state.hours} Hours {this.state.minutes} Minutes {this.state.seconds} Seconds</div>
+                <div>{this.state.days} Days {this.state.hours} Hours {this.state.minutes} Minutes {this.state.seconds} Seconds left until {this.state.date}!</div>
                 <h3>Enter a date</h3>
                 <input type="date" disabled={this.state.submitted} onChange={this.handleChange} value={this.state.date}></input>
                 <button disabled={this.state.submitted} onClick={this.getDate}>Start</button>
