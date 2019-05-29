0fa0f7d2e7df5af002bcf82b48e855b549af0b2c
Josh Doan <joshd@TagPay.com>
2019-05-29 15:34:40 -0700

    added holiday buttons

--- src/components/countdown/countdown.js
+++ src/components/countdown/countdown.js
@@ -11,7 +11,8 @@ const Countdown = class Countdown extends React.Component {
             hours: '',
             minutes: '',
             seconds: '',
-            submitted: false
+            submitted: false,
+            christmas: '12/25/2019'
         }
     }
 
@@ -19,16 +20,17 @@ const Countdown = class Countdown extends React.Component {
         this.setState({ date: event.target.value });
     };
 
-    getDate = () => {
+    countDown = (date) => {
+        clearInterval(this.interval);
         this.interval = setInterval(() => {
             const now = new Date().getTime() / 1000;
-            const date = new Date(this.state.date).getTime() / 1000;
-            const timeLeft = (date - now);
+            const target = new Date(date).getTime() / 1000;
+            const timeLeft = (target - now);
             const days = Math.floor(timeLeft / 86400);
             const hours = Math.floor(timeLeft / 3600) % 24;
             const minutes = Math.floor(timeLeft / 60) % 60;
             const seconds = Math.floor(timeLeft) % 60;
-            if (timeLeft <= 0) {
+            if (timeLeft <= 0 || !timeLeft) {
                 this.setState({ days: 0, hours: 0, minutes: 0, seconds: 0 })
                 this.setState({ submitted: false })
                 clearInterval(this.interval);
@@ -42,12 +44,20 @@ const Countdown = class Countdown extends React.Component {
     render() {
         return (
             <div className={styles.container}>
-                <div className={styles.display}><strong>{this.state.days}</strong> Days <strong>{this.state.hours}</strong> Hours <strong>{this.state.minutes}</strong> Minutes <strong>{this.state.seconds}</strong> Seconds left until <strong>{this.state.date}!</strong></div>
+                <div className={styles.display}><strong>{this.state.days}</strong> Days <strong>{this.state.hours}</strong> Hours <strong>{this.state.minutes}</strong> Minutes <strong>{this.state.seconds}</strong> Seconds left until <strong>{this.state.date}</strong>!</div>
                 <h3>Enter a date</h3>
                 <input className={styles.dateInput} type="date" disabled={this.state.submitted} onChange={this.handleChange} value={this.state.date}></input>
-                <br />
-                <button className={styles.startBtn} disabled={this.state.submitted} onClick={this.getDate}>Start</button>
-            </div >
+                <div>
+                    <button className={styles.btn} disabled={this.state.submitted} onClick={() => this.countDown(this.state.date)}>Start</button>
+                    <button className={styles.btn}>Reset</button>
+                </div>
+                <div>
+                    <button className={styles.btn}>New Years</button>
+                    <button className={styles.btn}>4th of July</button>
+                    <button className={styles.btn}>Thanksgiving</button>
+                    <button className={styles.btn} disabled={this.state.submitted} onClick={() => this.countDown(this.state.christmas)}>Christmas</button>
+                </div>
+            </div>
         );
     }
 }
