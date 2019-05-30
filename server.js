523406f5a19ab3d01408e984b43429f9eb7b40e8
Josh Doan <joshd@TagPay.com>
2019-05-30 15:41:31 -0700

    server v5

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', (req, res) => {
-        res.sendfile(path.join(__dirname = '/src/index.html'));
+        res.sendFile(path.join(__dirname = '/src/index.html'));
     })
 }
 
